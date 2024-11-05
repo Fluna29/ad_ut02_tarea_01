e83e524dceffb8447e4b85bcf6ca0f3fb47901ca
Fluna29 <fluna293107@gmail.com>
2024-11-05 19:59:58 +0100

    Delete of Actor class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
-
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.List;
