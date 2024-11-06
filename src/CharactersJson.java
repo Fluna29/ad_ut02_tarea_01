127cc3258d55634baed17e36fc6676913d3d5d04
Fluna29 <fluna293107@gmail.com>
2024-11-06 19:12:28 +0100

    Exercise 5 Complete, Added a repository called testJson with all the Xml divided by character.

--- src/CharactersJson.java
+++ src/CharactersJson.java
@@ -1,37 +1,137 @@
 import com.google.gson.JsonElement;
 import java.util.List;
-public class CharactersJson{
+import java.util.ArrayList;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlElementWrapper;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlType;
 
+@XmlRootElement(name = "character")
+//We use XmlType to define the order of the elements in the XML file
+@XmlType(propOrder = {
+        "characterName", "characterLink", "characterImageThumb", "characterImageFull",
+        "actorNameAsString", "actorLink", "actors", "houseNameAsString", "nickname", "royal", "kingsguard",
+        "parents", "parentOf", "guardianOf", "guardedBy", "siblings",
+        "marriedEngaged", "allies", "abducted", "killed", "killedBy", "serves", "servedBy"
+})
+public class CharactersJson {
 
+    @XmlElement(name = "character_name")
     private String characterName;
+
+    @XmlElement(name = "character_link")
     private String characterLink;
+
+    @XmlElement(name = "character_image_thumb")
     private String characterImageThumb;
+
+    @XmlElement(name = "character_image_full")
     private String characterImageFull;
-    private JsonElement actorName;  // It can be a String or an Actor object array
+
+    private JsonElement actorName;  // It can be a single String or an array of Strings
+
+    @XmlElement(name = "actor_name")
+    //We use this function to get the actor name when it is only one actor.
+    //If it is an array of actors, we will make use of the inner class Actor.
+    public String getActorNameAsString() {
+        if (actorName == null) return null;
+        if (actorName.isJsonPrimitive()) {
+            return actorName.getAsString();
+        }
+        return null;  // Returns null in case it's an array.
+    }
+
+    @XmlElement(name = "actor_link")
     private String actorLink;
-    private JsonElement houseName;  // It can be a String or a list of Strings
+
+    @XmlElementWrapper(name = "actors")
+    @XmlElement(name = "actor")
+    //This is used for the case when there are more than one actor for a character.
+    private List<Actor> actors;
+
+    private JsonElement houseName;  // It can be a single String or an array of Strings
+
+    @XmlElementWrapper(name = "houses")
+    @XmlElement(name = "house_name")
+    //We use this function to get the house name in a String or turn it in an Array of Strings called "houses".
+    public List<String> getHouseNameAsString() {
+        if (houseName == null) return null;
+        if (houseName.isJsonPrimitive()) {
+            return List.of(houseName.getAsString());  // Wrap the single string in a list
+        } else if (houseName.isJsonArray()) {
+            List<String> houses = new ArrayList<>();
+            houseName.getAsJsonArray().forEach(element -> houses.add(element.getAsString()));
+            return houses;
+        }
+        return null;
+    }
+
+    @XmlElement(name = "nickname")
     private String nickname;
+
+    @XmlElement(name = "royal")
     private Boolean royal;
+
+    @XmlElement(name = "kingsguard")
     private Boolean kingsguard;
+
+    @XmlElementWrapper(name = "parents")
+    @XmlElement(name = "name")
     private List<String> parents;
+
+    @XmlElementWrapper(name = "parent_of")
+    @XmlElement(name = "name")
     private List<String> parentOf;
+
+    @XmlElementWrapper(name = "guardian_of")
+    @XmlElement(name = "name")
     private List<String> guardianOf;
+
+    @XmlElementWrapper(name = "guarded_by")
+    @XmlElement(name = "name")
     private List<String> guardedBy;
+
+    @XmlElementWrapper(name = "siblings")
+    @XmlElement(name = "name")
     private List<String> siblings;
+
+    @XmlElementWrapper(name = "married_engaged")
+    @XmlElement(name = "name")
     private List<String> marriedEngaged;
+
+    @XmlElementWrapper(name = "allies")
+    @XmlElement(name = "name")
     private List<String> allies;
+
+    @XmlElementWrapper(name = "abducted")
+    @XmlElement(name = "name")
     private List<String> abducted;
+
+    @XmlElementWrapper(name = "killed")
+    @XmlElement(name = "name")
     private List<String> killed;
+
+    @XmlElementWrapper(name = "killed_by")
+    @XmlElement(name = "name")
     private List<String> killedBy;
+
+    @XmlElementWrapper(name = "serves")
+    @XmlElement(name = "name")
     private List<String> serves;
+
+    @XmlElementWrapper(name = "served_by")
+    @XmlElement(name = "name")
     private List<String> servedBy;
 
-    // Getters y Setters
+    // Getters & Setters
     public String getCharacterName() { return characterName; }
     public String getCharacterLink() { return characterLink; }
     public String getCharacterImageThumb() { return characterImageThumb; }
     public String getCharacterImageFull() { return characterImageFull; }
     public String getActorLink() { return actorLink; }
+    //We only put this getter for the case when there are more than one actor for a character.
+    //Becuase, in the case that is just one actor, we will use the function getActorNameAsString.
+    public List<Actor> getActors() { return actors; }
     public String getNickname() { return nickname; }
     public Boolean getRoyal() { return royal; }
     public Boolean getKingsguard() { return kingsguard; }
@@ -48,27 +148,21 @@ public class CharactersJson{
     public List<String> getServes() { return serves; }
     public List<String> getServedBy() { return servedBy; }
 
-    // Get houseName as String whether is a list of Strings or a single String
-    public String getHouseName() {
-        if (houseName == null) return null;
-        if (houseName.isJsonPrimitive()) {
-            return houseName.getAsString();
-        } else if (houseName.isJsonArray()) {
-            StringBuilder houses = new StringBuilder();
-            houseName.getAsJsonArray().forEach(element -> houses.append(element.getAsString()).append(", "));
-            return houses.toString().replaceAll(", $", "");
-        }
-        return null;
-    }
+    // Inner class for Actor
+    public static class Actor {
+        @XmlElement(name = "actor_name")
+        private String actorName;
 
-    // Get actorName as String whether is an Array or a single String
-    public String getActorNameAsString() {
-        if (actorName == null) return null;
-        if (actorName.isJsonPrimitive()) {
-            return actorName.getAsString();
-        }
-        return null;  // Returns null in case its is an array.
+        @XmlElement(name = "actor_link")
+        private String actorLink;
+
+        @XmlElementWrapper(name = "seasons_active")
+        @XmlElement(name = "season")
+        private List<Integer> seasonsActive;
+
+        // Getters & Setters
+        public String getActorName() { return actorName; }
+        public String getActorLink() { return actorLink; }
+        public List<Integer> getSeasonsActive() { return seasonsActive; }
     }
 }
-
-
--- src/Main.java
+++ src/Main.java
@@ -5,54 +5,88 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.List;
 import java.util.Map;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.Marshaller;
+import java.io.File;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
-            //We read the JSON from the URL
+            // We read the JSON from the URL
             String url = "https://raw.githubusercontent.com/jeffreylancaster/game-of-thrones/master/data/characters.json";
             List<CharactersJson> charactersList = readJSON(url);
 
             // For each character, we print one by one all the attributes
-            charactersList.forEach(character -> System.out.println(
-                    "Character Name: " + character.getCharacterName() +
-                            ", Character Link: " + character.getCharacterLink() +
-                            ", Character Image Thumb: " + character.getCharacterImageThumb() +
-                            ", Character Image Full: " + character.getCharacterImageFull() +
-                            ", Actor name / Actors names: " + character.getActorNameAsString() +
-                            ", Actor Link: " + character.getActorLink() +
-                            ", House Name: " + character.getHouseName() +
-                            ", Nickname: " + character.getNickname() +
-                            ", Royal: " + character.getRoyal() +
-                            ", Kingsguard: " + character.getKingsguard() +
-                            ", Parents: " + character.getParents() +
-                            ", Parent Of: " + character.getParentOf() +
-                            ", Guardian Of: " + character.getGuardianOf() +
-                            ", Guarded By: " + character.getGuardedBy() +
-                            ", Siblings: " + character.getSiblings() +
-                            ", Married Engaged: " + character.getMarriedEngaged() +
-                            ", Allies: " + character.getAllies() +
-                            ", Abducted: " + character.getAbducted() +
-                            ", Killed: " + character.getKilled() +
-                            ", Killed By: " + character.getKilledBy() +
-                            ", Serves: " + character.getServes() +
-                            ", Served By: " + character.getServedBy()
-            ));
+            charactersList.forEach(character -> {
+                System.out.println(
+                        "\nCharacter Name: " + character.getCharacterName() +
+                                "\n- Character Link: " + character.getCharacterLink() +
+                                "\n- Character Image Thumb: " + character.getCharacterImageThumb() +
+                                "\n- Character Image Full: " + character.getCharacterImageFull() +
+                                //Here we use the function actorsNames located in Main, to get the actor name or names
+                                "\n- Actor name / Actors names: " + actorsNames(character)+
+                                "\n- Actor Link: " + character.getActorLink() +
+                                "\n- House Name: " + character.getHouseNameAsString() +
+                                "\n- Nickname: " + character.getNickname() +
+                                "\n- Royal: " + character.getRoyal() +
+                                "\n- Kings Guard: " + character.getKingsguard() +
+                                "\n- Parents: " + character.getParents() +
+                                "\n- Parent Of: " + character.getParentOf() +
+                                "\n- Guardian Of: " + character.getGuardianOf() +
+                                "\n- Guarded By: " + character.getGuardedBy() +
+                                "\n- Siblings: " + character.getSiblings() +
+                                "\n- Married Engaged: " + character.getMarriedEngaged() +
+                                "\n- Allies: " + character.getAllies() +
+                                "\n- Abducted: " + character.getAbducted() +
+                                "\n- Killed: " + character.getKilled() +
+                                "\n- Killed By: " + character.getKilledBy() +
+                                "\n- Serves: " + character.getServes() +
+                                "\n- Served By: " + character.getServedBy()
+                );
+            });
+            for (CharactersJson character : charactersList) {
+                String filePath = "testJson/" + character.getCharacterName().replaceAll("\\s+", "_") + ".xml";
+                marshalling(character, filePath);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
+    };
 
-    //We use the Gson library to read the JSON from the URL we have to give to the method
+    // We use the Gson library to read the JSON from the URL we have to give to the method
     public static List<CharactersJson> readJSON(String url) throws Exception {
         Gson gson = new GsonBuilder().create();
         try (InputStreamReader reader = new InputStreamReader(new URL(url).openStream())) {
-            //Here we are using a TypeToken to specify that we are expecting a Map with a String as key and a List of CharactersJson as value
+            // Here we are using a TypeToken to specify that we are expecting a Map with a String as key and a List of CharactersJson as value
             Map<String, List<CharactersJson>> jsonMap = gson.fromJson(reader, new TypeToken<Map<String, List<CharactersJson>>>(){}.getType());
 
-            //The JSON has a root element called "characters" that contains the list of characters
+            // The JSON has a root element called "characters" that contains the list of characters
             return jsonMap.get("characters");
+        }
+    }
+
+    // We use JAXB to marshall the CharactersJson object into an XML file
+    public static void marshalling(Object character, String filePath) throws Exception {
+        JAXBContext context = JAXBContext.newInstance(CharactersJson.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        marshaller.marshal(character, new File(filePath));
+    }
+
+    //This method detects if it is a single actor or multiple actors and returns the names,
+    //works when data returns in Console, not in XML.
+    public static String actorsNames(CharactersJson character) {
+        if (character.getActors() == null || character.getActors().isEmpty()) {
+            return character.getActorNameAsString();
+        } else {
+            StringBuilder actorNames = new StringBuilder();
+            character.getActors().forEach(actor -> {
+                actorNames.append("Actor Name: ").append(actor.getActorName())
+                        .append(", Actor Link: ").append(actor.getActorLink())
+                        .append(", Seasons Active: ").append(actor.getSeasonsActive()).append("\n");
+            });
+            return actorNames.toString();
         }
     }
 }
